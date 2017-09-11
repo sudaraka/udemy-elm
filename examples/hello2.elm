79b6c93dbfb7b423c7d884ecf04d4131c1cce2f0
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-09-11 16:45:07 +0530

    Hello world 2

---
+++ examples/hello2.elm
@@ -0,0 +1,11 @@
+module HelloTwo exposing (..)
+
+import Html exposing (text, div, h1, p)
+
+
+main =
+    div []
+        [ h1 [] [ text "Welcome to my Elm Site !!!" ]
+        , p [] [ text "I am liking Elm so far." ]
+        , p [] [ text "Eger ti learn more about Elm." ]
+        ]
