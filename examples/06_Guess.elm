b554811bc11def95bf1e734993a993a4ed53ce94
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-09-16 15:57:26 +0530

    Guessing game: add word list to the game

--- examples/06_Guess.elm
+++ examples/06_Guess.elm
@@ -26,18 +26,25 @@ type alias Model =
     , guess : String
     , revealedWord : RevealedWord
     , result : Result
+    , wordList : List String
     }
 
 
 model : Model
 model =
-    Model "Saturday" "" (RevealedWord "S" 2) (Result "" False)
+    Model "Saturday" "" (RevealedWord "S" 2) (Result "" False) initialWordList
+
+
+initialWordList : List String
+initialWordList =
+    [ "Banana", "Kitten", "Orangutan", "Italic", "Paperclip", "Afternoon" ]
 
 
 type Msg
     = Answer String
     | Reveal
     | Check
+    | Another
 
 
 update : Msg -> Model -> Model
@@ -51,6 +58,26 @@ update msg model =
 
         Check ->
             { model | result = checkResult model }
+
+        Another ->
+            let
+                newWord =
+                    getNewWord model
+            in
+                Model
+                    newWord
+                    ""
+                    (RevealedWord (String.slice 0 1 newWord) 2)
+                    (Result "" False)
+                    (List.drop 1 model.wordList)
+
+
+getNewWord : Model -> String
+getNewWord { word, wordList } =
+    wordList
+        |> List.filter (\word -> word /= model.word)
+        |> List.take 1
+        |> String.concat
 
 
 checkResult : Model -> Result
@@ -84,6 +111,7 @@ view model =
         , div []
             [ button [ onClick Reveal ] [ text "Give me a hint" ]
             , button [ onClick Check ] [ text "Submit Answer" ]
+            , button [ onClick Another ] [ text "Another Word" ]
             ]
         , generateResult model
         ]
